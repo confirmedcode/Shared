d3aeeea52255116d1d61c1cf034c2065c2523695
Maxim Golitsyn <maxim.golitsyn@joinappex.com>
2023-01-09 23:07:45 +0300

    Fix PG package

--- utilities/database.js
+++ utilities/database.js
@@ -14,7 +14,7 @@ const pool = new Pool({
   user: PG_USER,
   password: PG_PASSWORD,
   port: 5432,
-  ssl: process.env.ENVIRONMENT === "LOCAL" ? false : { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
   max: 20,
   min: 4,
   idleTimeoutMillis: 1000,
